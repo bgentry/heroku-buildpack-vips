c8bd85f4ecfd9f0b419ca9c1de53c684934924ff
Blake Gentry <bgentry@users.noreply.github.com>
2015-08-12 22:07:42 -0700

    remove prefix, add them both, why not...?

--- .profile.d/vips.sh
+++ .profile.d/vips.sh
@@ -1,3 +1,3 @@
 export PATH="$PATH:$BUILD_DIR/vendor/vips/bin"
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$BUILD_DIR/vendor/vips/lib/pkgconfig"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BUILD_DIR/vendor/vips/lib"
+export PKG_CONFIG_PATH="$BUILD_DIR/vendor/vips/lib/pkgconfig:$APP_DIR/vendor/vips/lib/pkgconfig"
+export LD_LIBRARY_PATH="$BUILD_DIR/vendor/vips/lib:$APP_DIR/vendor/vips/lib"
